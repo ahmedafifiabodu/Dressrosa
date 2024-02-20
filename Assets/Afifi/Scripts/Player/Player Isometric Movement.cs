5f09856faf2644fb6b06a09f60527556c248350e
AlaaaEzzat <alaaazzetdd@outlook.sa>
2024-02-20 09:46:07 +0200

    Squashed commit of the following:
    commit a59eb69847e919f8c0ab102c21c677302ad27039
    Author: Ahmed Afifi Abdou <aafifi1988@icloud.com>
    Date:   Mon Feb 19 21:52:34 2024 +0200
    
        -Clean/Deleted miscellaneous scripts.
        -Updated the Quest System.
        -Added Interactable System + its UI.
        -Added a mini inventory script to test the interactable system.
        -Added the Interactable button (E).
    
    commit 4be08ba444bb5c977f54457a3f1da7afef1d4048
    Author: Ahmed Afifi Abdou <aafifi1988@icloud.com>
    Date:   Mon Feb 19 19:13:58 2024 +0200
    
        -Added The Porject to the main branch
    
    commit 1ac2a570ed7267d4caecb11f675c428c02a84590
    Author: Ahmed Afifi Abdou <aafifi1988@icloud.com>
    Date:   Mon Feb 19 19:08:24 2024 +0200
    
        -Added Isometeric movement
        -Added Audio System
        -Added Quest System
    
    commit 023d64f448ab75f1233059ebb590b57d2bf47508
    Author: Ahmed Afifi Abdou <aafifi1988@icloud.com>
    Date:   Sun Feb 18 23:09:10 2024 +0200
    
        -Added Stamina Bar (Can also be used as an Energy Bar).
        -Implemented Reduced Movement Speed when the player is out of stamina (The reduced speed will also affect the player's animation speed).
    
    commit ac4e7810235653d50b915a5994484e2d6c4b6839
    Author: Ahmed Afifi Abdou <aafifi1988@icloud.com>
    Date:   Sun Feb 18 22:34:03 2024 +0200
    
        -Implemented the New Input System (Hardcoded).
        -Integrated the Default UI Input with the custom Input System.
        -Added Movement Animation (8 Directions).
        -Integrated Cinema Machine to follow the player.
        -Implemented a New Custom Zoom and Zoom Out feature for Cinema Machine (Demo).
        -Set up Camera Bounds for the Camera (Currently Not Functional).

---
+++ Assets/Afifi/Scripts/Player/Player Isometric Movement.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+
+public class PlayerIsometricMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _speed = 5f;
+    [Range(0, 0.9f)][SerializeField] private float _crippledSpeed = 0.5f;
+    [SerializeField] private Animator _animator;
+
+    private Vector2 lastMoveDirection;
+    private bool facingLeft = false;
+    private PlayerInformation _playerInformation;
+    private float _timeSinceLastMove = 0;
+
+    private void Start() => _playerInformation = PlayerInformation.Instance;
+
+    internal void ProcessMove(Vector2 _input)
+    {
+        //To Make the player to stop from movement when out of stamina
+        /*        if (_playerInformation.IsOutOfStamina)
+                {
+                    _input = Vector2.zero;
+                }
+                else
+                {
+                    float moveX = _input.x;
+                    float moveY = _input.y;
+
+                    if (moveX != 0 || moveY != 0)
+                    {
+                        lastMoveDirection = _input;
+                        _playerInformation.DecreaseStamina(Time.fixedDeltaTime);
+                    }
+                }*/
+
+        float moveX = _input.x;
+        float moveY = _input.y;
+
+        // Adjust for isometric movement
+        float isoMoveX = (moveX - moveY) / 2;
+        float isoMoveY = (moveX + moveY) / 2;
+
+        Vector2 isoInput = new(isoMoveX, isoMoveY);
+
+        if (isoMoveX != 0 || isoMoveY != 0)
+        {
+            lastMoveDirection = isoInput;
+            _playerInformation.DecreaseStamina(Time.fixedDeltaTime);
+            _timeSinceLastMove = 0;
+        }
+        else
+        {
+            _timeSinceLastMove += Time.fixedDeltaTime;
+            if (_timeSinceLastMove >= 0.5f)
+            {
+                _playerInformation.RechargeStamina(Time.fixedDeltaTime);
+            }
+        }
+
+        if (_playerInformation.IsOutOfStamina)
+        {
+            isoInput *= 0.5f;
+            _animator.speed = 0.5f;
+        }
+        else
+        {
+            _animator.speed = 1f;
+        }
+
+        _rb.velocity = isoInput * _speed;
+        Animate(isoInput);
+
+        //if (_input.x < 0 && !facingLeft || _input.x > 0 && facingLeft)
+        //{
+        //    Flip();
+        //}
+    }
+
+    private void Animate(Vector2 _input)
+    {
+        _animator.SetFloat("Move X", _input.x);
+        _animator.SetFloat("Move Y", _input.y);
+
+        _animator.SetFloat("Move Magnitude", _input.magnitude);
+
+        _animator.SetFloat("Last Move X", lastMoveDirection.x);
+        _animator.SetFloat("Last Move Y", lastMoveDirection.y);
+    }
+
+    private void Flip()
+    {
+        Vector3 _scale = transform.localScale;
+        _scale.x *= -1;
+        transform.localScale = _scale;
+        facingLeft = !facingLeft;
+    }
+
+    internal void Fire()
+    {
+        Debug.Log("Fire");
+    }
+}
